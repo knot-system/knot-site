21dbc72a14ddf5c4e0d30288692d31ef81fa292e
maxhaesslein <git@maxhaesslein.de>
2022-11-08 13:27:01 +0100

    (very) basic css

--- site/index.php
+++ site/index.php
@@ -20,28 +20,33 @@ ___________.__                     .__           .__
 	<link rel="token_endpoint" href="https://tokens.indieauth.com/token" />
 	<link rel="me authn" href="mailto:<?= \Eigenheim\Config::getConfig('auth_mail') ?>">
 	<link rel="micropub" href="<?= \Eigenheim\Micropub::getEndpoint() ?>" />
+	<link rel="stylesheet" href="<?= EH_BASEURL ?>assets/css/global.css?v=<?= \Eigenheim\Core::getVersion() ?>">
 </head>
 <body>
+<header>
 	<h1>Eigenheim</h1>
+</header>
+<main>
 <?php
 
-$files = \Eigenheim\Files::read_dir( '' );
+	$files = \Eigenheim\Files::read_dir( '' );
 
-foreach( $files as $filename ){
+	foreach( $files as $filename ){
 
-	$file_contents = \Eigenheim\Files::read_file( $filename );
+		$file_contents = \Eigenheim\Files::read_file( $filename );
 
-	$title = $file_contents['name'];
-	$text = \Eigenheim\Text::auto_p($file_contents['content']);
+		$title = $file_contents['name'];
+		$text = \Eigenheim\Text::auto_p($file_contents['content']);
 
 	?>
 	<hr>
 	<h2><?= $title ?></h2>
 	<?= $text ?>
-	<?php
-}
-?>
-<hr>
-
+<?php
+	}
+	?>
+	<hr>
+</main>
+<footer>Eigenheim v.<?= \Eigenheim\Core::getVersion() ?></footer>
 </body>
 </html>
---
+++ system/functions/core.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Eigenheim;
+
+if( ! defined( 'EH_ABSPATH' ) ) exit;
+
+class Core {
+	
+	static function getVersion(){
+
+		$version = 'alpha.1';
+
+		return $version;
+	}
+
+}
