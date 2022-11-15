561e0d480bd0bd4210613ee27e47f7d1bf1ea1c5
maxhaesslein <git@maxhaesslein.de>
2022-11-15 12:23:34 +0100

    basic microformats support for articles

--- site/snippets/post.php
+++ site/snippets/post.php
@@ -10,10 +10,10 @@ $date = date( 'd.m.Y', $post['timestamp'] );
 $tags = $post['tags'];
 
 ?>
-<section>
+<article class="h-entry">
 	<a class="anchor" name="<?= $post['id'] ?>"></a>
-	<small><?= $date ?></small>
-	<?php if( $title ) echo '<h2>'.$title.'</h2>'; ?>
-	<?= $text; ?>
-	<?php if( count($tags) ) echo '<ul class="tags"><li>'.implode( '</li><li>', $tags).'</li></ul>'; ?>
-</section>
+	<time class="dt-published" datetime="<?= $date ?>"><?= $date ?></time>
+	<?php if( $title ) echo '<h2 class="p-name">'.$title.'</h2>'; ?>
+	<?php if( $text ) echo '<div class="e-content">'.$text.'</div>'; ?>
+	<?php if( count($tags) ) echo '<ul class="tags"><li class="p-category">'.implode( '</li><li>', $tags).'</li></ul>'; ?>
+</article>
