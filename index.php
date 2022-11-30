9a12e74100e0e649749e2e694d4d97ea7a6c057f
maxhaesslein <git@maxhaesslein.de>
2022-11-30 10:59:55 +0100

    added load.php

--- index.php
+++ index.php
@@ -1,33 +1,3 @@
 <?php
 
-include( 'system/classes.php' );
-
-global $eigenheim;
-$eigenheim = new Eigenheim();
-
-include( 'system/functions.php' );
-
-
-if( ! file_exists($eigenheim->abspath.'config.php') || ! file_exists($eigenheim->abspath.'.htaccess') || isset($_GET['setup']) ) {
-	$eigenheim->include( 'system/setup.php' );
-	exit;
-}
-
-
-if( file_exists($eigenheim->abspath.'update') || file_exists($eigenheim->abspath.'update.txt') ) {
-	$eigenheim->include( 'system/update.php' );
-	exit;
-}
-
-
-$theme = get_theme();
-$eigenheim->include( $theme['_path'].'functions.php' );
-
-
-$route = get_route();
-
-$template = $route['template'];
-$args = false;
-if( ! empty($route['args']) ) $args = $route['args'];
-
-$eigenheim->include( 'system/site/'.$template.'.php', $args );
+include_once( 'system/load.php' );
