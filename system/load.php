d760c7a01ee14ff981d1d8cfcab27bf16c37c90b
maxhaesslein <git@maxhaesslein.de>
2022-12-05 17:57:33 +0100

    fixed the setup process; it no longer needs classes or functions to be available

--- system/load.php
+++ system/load.php
@@ -3,19 +3,22 @@
 $abspath = realpath(dirname(__FILE__)).'/';
 $abspath = preg_replace( '/system\/$/', '', $abspath );
 
+
+if( ! file_exists($abspath.'config.php')
+ || ! file_exists($abspath.'.htaccess')
+ || isset($_GET['setup'])
+) {
+	include_once( $abspath.'system/setup.php' );
+	exit;
+}
+
+
 include( 'system/functions.php' );
 include( 'system/classes.php' );
 
 $eigenheim = new Eigenheim();
 
 
-if( ! file_exists($eigenheim->abspath.'config.php')
- || ! file_exists($eigenheim->abspath.'.htaccess')
- || isset($_GET['setup'])
-) {
-	$eigenheim->include( 'system/setup.php' );
-	exit;
-}
 
 if( isset($_GET['update']) && (file_exists($eigenheim->abspath.'update')
  || file_exists($eigenheim->abspath.'update.txt'))
--- system/setup.php
+++ system/setup.php
@@ -1,10 +1,17 @@
 <?php
 
-// this file creates some basic files and folderstructure and gets called, if no config.php exists yet.
+// this file creates some basic files and folderstructure and gets called, if important files are missing (like the config.php or .htaccess)
 
-if( ! $eigenheim ) exit;
 
-if( file_exists($eigenheim->abspath.'config.php') && file_exists($eigenheim->abspath.'.htaccess') ) {
+$basefolder = str_replace( 'index.php', '', $_SERVER['PHP_SELF']);
+
+if( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ) $baseurl = 'https://';
+else $baseurl = 'http://';
+$baseurl .= $_SERVER['HTTP_HOST'];
+$baseurl .= $basefolder;
+
+
+if( file_exists($abspath.'config.php') && file_exists($abspath.'.htaccess') ) {
 	?>
 	<p>Setup already finished. Please delete <em>config.php</em> or <em>.htaccess</em> from the root directory to re-run the setup.</p>
 	<?php
@@ -12,33 +19,33 @@ if( file_exists($eigenheim->abspath.'config.php') && file_exists($eigenheim->abs
 }
 
 ?>
-<p>Hi. This is the first-time setup of Eigenheim <?= $eigenheim->version() ?>.</p>
+<p>Hi. This is the first-time setup of Eigenheim.</p>
 <p>We create some files and folders to get everything up and running.</p>
 
 <hr>
 
 <h3>Environment:</h3>
 <ul>
-	<li>ABSPATH: <em><?= $eigenheim->abspath ?></em></li>
-	<li>BASEFOLDER: <em><?= $eigenheim->basefolder ?></em></li>
-	<li>BASEURL: <em><?= $eigenheim->baseurl ?></em></li>
+	<li>ABSPATH: <em><?= $abspath ?></em></li>
+	<li>BASEFOLDER: <em><?= $basefolder ?></em></li>
+	<li>BASEURL: <em><?= $baseurl ?></em></li>
 </ul>
 
 <?php
 
-if( $eigenheim->abspath == '' ) {
+if( $abspath == '' ) {
 	?>
 	<p><strong>ERROR:</strong> ABSPATH is empty. we don't know what went wrong. we abort the setup here.</p>
 	<?php
 	exit;
 }
-if( $eigenheim->basefolder == '' ) {
+if( $basefolder == '' ) {
 	?>
 	<p><strong>ERROR:</strong> BASEFOLDER is empty. we don't know what went wrong. we abort the setup here.</p>
 	<?php
 	exit;
 }
-if( $eigenheim->baseurl == '' ) {
+if( $baseurl == '' ) {
 	?>
 	<p><strong>ERROR:</strong> BASEURL is empty. we don't know what went wrong. we abort the setup here.</p>
 	<?php
@@ -46,7 +53,7 @@ if( $eigenheim->baseurl == '' ) {
 }
 
 $config = true;
-if( file_exists($eigenheim->abspath.'config.php') ) $config = false;
+if( file_exists($abspath.'config.php') ) $config = false;
 
 if( $config && 
 	( empty($_POST['auth_mail']) || empty($_POST['author_name']) || empty($_POST['site_title']) ) 
@@ -54,11 +61,11 @@ if( $config &&
 	?>
 	<hr>
 	<p>Please fill out these fields:</p>
-	<form action="<?= $eigenheim->baseurl ?>" method="POST">
+	<form action="<?= $baseurl ?>" method="POST">
 		<p><label><strong>Site Title</strong><br><input type="text" name="site_title" required></label></p>
 		<p><label><strong>Authorization Mail</strong><br><small>(this is were we send the login token to, when you log into a micropub client. It is not displayed publicly, but is added to the HTML source code)</small><br><input type="email" name="auth_mail" required></label></p>
 		<p><label><strong>Author Name</strong><br><input type="text" name="author_name" required></label></p>
-		<p><small>(all fields are required)</small></p>
+		<p><small>(all fields above are required)</small></p>
 		<p><label><input type="checkbox" name="testcontent" value="true" checked>create test content</label>
 		<p><button>start installation</button></p>
 	</form>
@@ -82,14 +89,14 @@ if( $config ) {
 <ul>
 	<li>checking if <em>.htaccess</em> file exists</li>
 <?php
-if( ! file_exists( $eigenheim->abspath.'.htaccess' ) ) {
-	$rewrite_base = $eigenheim->basefolder;
+if( ! file_exists( $abspath.'.htaccess' ) ) {
+	$rewrite_base = $basefolder;
 	if( $rewrite_base == '' ) $rewrite_base = '/';
 	?>
 	<li>file <em>.htaccess</em> does not exist, creating it with rewrite base <em><?= $rewrite_base ?></em></li>
 	<?php
 	$content = "<IfModule mod_rewrite.c>\r\nRewriteEngine on\r\nRewriteBase ".$rewrite_base."\r\n\r\nRewriteRule (^|/)\.(?!well-known\/) index.php [L]\r\nRewriteRule ^content/(.*)\.(txt|md|mdown)$ index.php [L]\r\nRewriteRule ^content/(.*)\.(jpg|jpeg|png)$ index.php [L]\r\nRewriteRule ^system/(.*) index.php [L]\r\nRewriteRule ^log/(.*) index.php [L]\r\nRewriteRule ^cache/(.*) index.php [L]\r\n\r\nRewriteCond %{REQUEST_FILENAME} !-d\r\nRewriteCond %{REQUEST_FILENAME} !-f\r\nRewriteRule . index.php [L]\r\n</IfModule>";
-	if( file_put_contents( $eigenheim->abspath.'.htaccess', $content ) === false ) {
+	if( file_put_contents( $abspath.'.htaccess', $content ) === false ) {
 		?>
 		<li><strong>ERROR:</strong> file <em>.htaccess</em> could not be created. Please check the permissions of the root folder and make sure we are allowed to write to it. we abort the setup here.</li>
 		<?php
@@ -110,11 +117,11 @@ if( ! file_exists( $eigenheim->abspath.'.htaccess' ) ) {
 <h3>checking <em>content/</em> folder:</h3>
 <ul>
 <?php
-if( ! is_dir( $eigenheim->abspath.'content/') ) {
+if( ! is_dir( $abspath.'content/') ) {
 	?>
 	<li>folder <em>content/</em> does not exist, trying to create it</li>
 	<?php
-	if( mkdir( $eigenheim->abspath.'content/', 0777, true ) === false ) {
+	if( mkdir( $abspath.'content/', 0777, true ) === false ) {
 		?>
 		<li><strong>ERROR:</strong> folder <em>content/</em> could not be created. Please check the permissions of the root folder and make sure we are allowed to write to it. we abort the setup here.</li>
 		<?php
@@ -134,11 +141,11 @@ if( ! is_dir( $eigenheim->abspath.'content/') ) {
 <h3>checking <em>cache/</em> folder:</h3>
 <ul>
 <?php
-if( ! is_dir( $eigenheim->abspath.'cache/') ) {
+if( ! is_dir( $abspath.'cache/') ) {
 	?>
 	<li>folder <em>cache/</em> does not exist, trying to create it</li>
 	<?php
-	if( mkdir( $eigenheim->abspath.'cache/', 0777, true ) === false ) {
+	if( mkdir( $abspath.'cache/', 0777, true ) === false ) {
 		?>
 		<li><strong>ERROR:</strong> folder <em>cache/</em> could not be created. Please check the permissions of the root folder and make sure we are allowed to write to it. we abort the setup here.</li>
 		<?php
@@ -162,9 +169,6 @@ if( $testcontent ) {
 	<ul>
 	<?php
 	function setup_create_testcontent( $root, $file_structure ) {
-
-		global $eigenheim;
-
 		if( $file_structure['type'] == 'file' ) {
 			$filename = $file_structure['name'];
 			$data = $file_structure['content'];
@@ -210,6 +214,8 @@ if( $testcontent ) {
 		}
 	}
 
+	$timestamp = time();
+
 	$file_structure = array(
 		'type' => 'root',
 		'name' => '',
@@ -243,39 +249,39 @@ if( $testcontent ) {
 					array(
 						'type' => 'folder',
 						'name' => 'posts',
+						'content' => array(
+							array(
+								'type' => 'folder',
+								'name' => date('Y', $timestamp),
+								'content' => array(
+									array(
+										'type' => 'folder',
+										'name' => date('m', $timestamp),
+										'content' => array(
+											array(
+												'type' => 'folder',
+												'name' => date('Y-m-d_H-i-s', $timestamp).'_testpost',
+												'content' => array(
+													array(
+														'type' => 'file',
+														'name' => 'post.txt',
+														'content' => "h: entry\r\n\r\n----\r\n\r\nname: Testpost\r\n\r\n----\r\n\r\ncontent: This is a first testpost. It is saved at <em>content/posts/".date('Y', $timestamp)."/".date('m', $timestamp)."/".date('Y-m-d_H-i-s', $timestamp)."_testpost/post.txt</em>\r\n\r\n----\r\n\r\npost-status: published\r\n\r\n----\r\n\r\ncategory: [\"eigenheim\",\"testpost\"]\r\n\r\n----\r\n\r\nslug: testpost\r\n\r\n----\r\n\r\ntimestamp: ".$timestamp."\r\n\r\n----\r\n\r\ndate: ".date('c', $timestamp)."\r\n\r\n----\r\n\r\nid: testpost"
+													)
+												)
+											)
+										)
+									)
+								)
+							)
+						)
 					),
 				)
 			),
 		),
 	);
-	setup_create_testcontent( $eigenheim->abspath, $file_structure );
+	setup_create_testcontent( $abspath, $file_structure );
 	?>
 	<li>test folder structure created successfully</li>
-	<?php
-	if( ! is_dir($eigenheim->abspath.'content/posts/'.date('Y')) ) {
-		?>
-		<li>creating test post</li>
-		<?php
-		$testpost_data = array(
-			'timestamp' => time(),
-			'date' => date('c', time()),
-			'category' => json_encode(array('testpost')),
-			'post-status' => 'published',
-			'name' => 'Testpost',
-			'content' => 'This is a first testpost. It is saved at <em>content/posts/'.date('Y').'/'.date('m').'/</em>.'
-		);
-		if( ! create_post_in_database( $testpost_data ) ) {
-			?>
-			<li><strong>ERROR:</strong> could not create testpost. we abort the setup here.</li>
-			<?php
-			exit;
-		} else {
-			?>
-			<li>test post created successfully</li>
-			<?php
-		}
-	}
-	?>
 	</ul>
 	<?php
 }
@@ -286,8 +292,8 @@ if( $testcontent ) {
 <ul>
 	<?php
 	if( $config ) {
-		$content = "<?php\r\n\r\nreturn [\r\n	'site_title' => '".$site_title."',\r\n	'auth_mail' => '".$auth_mail."',\r\n	'debug' => true,\r\n	'author' => [\r\n		'p-name' => '".$author_name."',\r\n	],\r\n];\r\n";
-		if( file_put_contents( $eigenheim->abspath.'config.php', $content ) === false ) {
+		$content = "<?php\r\n\r\nreturn [\r\n	'site_title' => '".$site_title."',\r\n	'auth_mail' => '".$auth_mail."',\r\n	'debug' => true,\r\n	'author' => [\r\n		'p-name' => '".$author_name."',\r\n	],\r\n];\r\n"; // TODO: remove the debug option, when the system is stable enough
+		if( file_put_contents( $abspath.'config.php', $content ) === false ) {
 			?>
 			<li><strong>ERROR:</strong> could not create the file <em>config.php</em>. make sure the folder is writeable. we abort the setup here.</li>
 			<?php
@@ -307,7 +313,7 @@ if( $testcontent ) {
 
 <hr>
 <h3>Setup finished!</h3>
-<p>please <a href="<?= $eigenheim->baseurl ?>">reload this page</a>.</p>
+<p>please <a href="<?= $baseurl ?>">reload this page</a>.</p>
 <hr>
 <?php
 exit;
