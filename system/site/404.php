a09e05f7b85d7911fa9d5bb4b7cc1dc976aef97c
maxhaesslein <git@maxhaesslein.de>
2022-12-07 14:50:36 +0100

    added 404 snippet

--- system/site/404.php
+++ system/site/404.php
@@ -6,11 +6,6 @@ http_response_code(404);
 
 snippet( 'header' );
 
-?>
-<article>
-	<h2>Nothing here :(</h2>
-	<p>This page does not exist. Maybe it was deleted, or did not even exist in the first place. Have a look at the <a href="<?= url() ?>">overview</a> instead!</p>
-</article>
-<?php
+snippet( '404' );
 
 snippet( 'footer' );
---
+++ system/site/snippets/404.php
@@ -0,0 +1,11 @@
+<?php
+
+// Version: alpha.11
+
+if( ! $eigenheim ) exit;
+
+?>
+<article>
+	<h2>Nothing here :(</h2>
+	<p>This page does not exist. Maybe it was deleted, or did not even exist in the first place. Have a look at the <a href="<?= url() ?>">overview</a> instead!</p>
+</article>
