803fabe93f1be18c55eb1baaabfa2e9223efc4ae
maxhaesslein <git@maxhaesslein.de>
2023-01-09 16:00:55 +0100

    default theme favicon

--- theme/default/functions.php
+++ theme/default/functions.php
@@ -13,3 +13,7 @@ add_metatag( 'font_preload_patua-one-400', '<link rel="preload" href="'.$eigenhe
 // change the 'generator' meta-tag to include the current theme:
 remove_metatag( 'generator' );
 add_metatag( 'generator', '<meta tag="generator" content="Eigenheim v.'.$eigenheim->version().' with '.$eigenheim->theme->get('name').' v.'.$eigenheim->theme->get('version').'">' );
+
+
+// add favicon
+add_metatag( 'favicon', '<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🏡</text></svg>">' );
