760434bdf69bc2181724323a23c467abf9d1b59d
maxhaesslein <git@maxhaesslein.de>
2023-03-07 16:51:22 +0100

    setup.php now runs silently in the background, if config exists

--- system/setup.php
+++ system/setup.php
@@ -3,6 +3,11 @@
 // this file creates some basic files and folderstructure and gets called, if important files are missing (like the config.php or .htaccess)
 
 
+$output = false;
+$debug_output = true;
+
+if( isset($_REQUEST['debug']) ) $output = true;
+
 $basefolder = str_replace( 'index.php', '', $_SERVER['PHP_SELF']);
 
 if( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ) $baseurl = 'https://';
@@ -12,43 +17,60 @@ $baseurl .= $basefolder;
 
 
 if( file_exists($abspath.'config.php') && file_exists($abspath.'.htaccess') ) {
-	?>
-	<p>Setup already finished. Please delete <em>config.php</em> or <em>.htaccess</em> from the root directory to re-run the setup.</p>
-	<?php
-	exit;
+
+	if( $output ) {
+		?>
+		<p>Setup already finished. Please delete <em>config.php</em> or <em>.htaccess</em> from the root directory to re-run the setup.</p>
+		<?php
+	}
+	return;
 }
 
-?>
-<p>Hi. This is the first-time setup of Eigenheim.</p>
-<p>We create some files and folders to get everything up and running.</p>
+if( $output ) {
+	?>
+	<p>Hi. This is the first-time setup of Eigenheim.</p>
+	<p>We create some files and folders to get everything up and running.</p>
 
-<hr>
+	<hr>
 
-<h3>Environment:</h3>
-<ul>
-	<li>ABSPATH: <em><?= $abspath ?></em></li>
-	<li>BASEFOLDER: <em><?= $basefolder ?></em></li>
-	<li>BASEURL: <em><?= $baseurl ?></em></li>
-</ul>
+	<h3>Environment:</h3>
+	<ul>
+		<li>ABSPATH: <em><?= $abspath ?></em></li>
+		<li>BASEFOLDER: <em><?= $basefolder ?></em></li>
+		<li>BASEURL: <em><?= $baseurl ?></em></li>
+	</ul>
 
-<?php
+	<?php
+}
 
 if( $abspath == '' ) {
-	?>
-	<p><strong>ERROR:</strong> ABSPATH is empty. we don't know what went wrong. we abort the setup here.</p>
-	<?php
+
+	if( $debug_output ) {
+		?>
+		<p><strong>ERROR:</strong> ABSPATH is empty. we don't know what went wrong. we abort the setup here.</p>
+		<?php
+	}
+
 	exit;
 }
 if( $basefolder == '' ) {
-	?>
-	<p><strong>ERROR:</strong> BASEFOLDER is empty. we don't know what went wrong. we abort the setup here.</p>
-	<?php
+
+	if( $debug_output ) {
+		?>
+		<p><strong>ERROR:</strong> BASEFOLDER is empty. we don't know what went wrong. we abort the setup here.</p>
+		<?php
+	}
+
 	exit;
 }
 if( $baseurl == '' ) {
-	?>
-	<p><strong>ERROR:</strong> BASEURL is empty. we don't know what went wrong. we abort the setup here.</p>
-	<?php
+
+	if( $debug_output ) {
+		?>
+		<p><strong>ERROR:</strong> BASEURL is empty. we don't know what went wrong. we abort the setup here.</p>
+		<?php
+	}
+
 	exit;
 }
 
@@ -81,128 +103,219 @@ if( $config ) {
 	if( ! empty($_POST['testcontent']) ) $testcontent = true;
 }
 
-?>
 
-<hr>
+if( $output ) {
+	?>
 
-<h3>checking <em>.htaccess</em> file:</h3>
-<ul>
-	<li>checking if <em>.htaccess</em> file exists</li>
-<?php
+	<hr>
+
+	<h3>checking <em>.htaccess</em> file:</h3>
+	<ul>
+		<li>checking if <em>.htaccess</em> file exists</li>
+	<?php
+}
+
 if( ! file_exists( $abspath.'.htaccess' ) ) {
 	$rewrite_base = $basefolder;
 	if( $rewrite_base == '' ) $rewrite_base = '/';
-	?>
-	<li>file <em>.htaccess</em> does not exist, creating it with rewrite base <em><?= $rewrite_base ?></em></li>
-	<?php
+
+	if( $output ) {
+		?>
+		<li>file <em>.htaccess</em> does not exist, creating it with rewrite base <em><?= $rewrite_base ?></em></li>
+		<?php
+	}
+
 	$content = "<IfModule mod_rewrite.c>\r\nRewriteEngine on\r\nRewriteBase ".$rewrite_base."\r\n\r\nRewriteRule ^theme/[^/]+/assets/(.*)$ - [L]\r\nRewriteRule ^system/site/assets/(.*)$ - [L]\r\nRewriteRule (^|/)\.(?!well-known\/) index.php [L]\r\nRewriteRule ^content/(.*)\.(txt|md|mdown)$ index.php [L]\r\nRewriteRule ^content/(.*)\.(jpg|jpeg|png)$ index.php [L]\r\nRewriteRule ^theme/(.*) index.php [L]\r\nRewriteRule ^system/(.*) index.php [L]\r\nRewriteRule ^log/(.*) index.php [L]\r\nRewriteRule ^cache/(.*) index.php [L]\r\n\r\nRewriteCond %{REQUEST_FILENAME} !-d\r\nRewriteCond %{REQUEST_FILENAME} !-f\r\nRewriteRule . index.php [L]\r\n</IfModule>";
 	if( file_put_contents( $abspath.'.htaccess', $content ) === false ) {
-		?>
-		<li><strong>ERROR:</strong> file <em>.htaccess</em> could not be created. Please check the permissions of the root folder and make sure we are allowed to write to it. we abort the setup here.</li>
-		<?php
+
+		if( $output ) {
+			?>
+			<li><strong>ERROR:</strong> file <em>.htaccess</em> could not be created. Please check the permissions of the root folder and make sure we are allowed to write to it. we abort the setup here.</li>
+			<?php
+		}
+
 		exit;
 	} else {
-		?>
-		<li>file <em>.htaccess</em> was successfully created</li>
-		<?php
+
+		if( $output ) {
+			?>
+			<li>file <em>.htaccess</em> was successfully created</li>
+			<?php
+		}
+
 	}
 } else {
-	?>
-	<li>file <em>.htaccess</em> exists; if you need to recreate it, delete it and rerun this setup.</li>
-	<?php
-}
-?>
-</ul>
 
-<h3>checking <em>content/</em> folder:</h3>
-<ul>
-<?php
-if( ! is_dir( $abspath.'content/') ) {
-	?>
-	<li>folder <em>content/</em> does not exist, trying to create it</li>
-	<?php
-	if( mkdir( $abspath.'content/', 0777, true ) === false ) {
+	if( $output ) {
 		?>
-		<li><strong>ERROR:</strong> folder <em>content/</em> could not be created. Please check the permissions of the root folder and make sure we are allowed to write to it. we abort the setup here.</li>
-		<?php
-		exit;
-	} else {
-		?>
-		<li>folder <em>content/</em> was created successfully</li>
+		<li>file <em>.htaccess</em> exists; if you need to recreate it, delete it and rerun this setup.</li>
 		<?php
 	}
-} else {
-	?><li>folder <em>content/</em> already exists, we do not need to create it</li><?php
+
 }
-?>
-</ul>
 
-
-<h3>checking <em>cache/</em> folder:</h3>
-<ul>
-<?php
-if( ! is_dir( $abspath.'cache/') ) {
+if( $output ) {
 	?>
-	<li>folder <em>cache/</em> does not exist, trying to create it</li>
-	<?php
-	if( mkdir( $abspath.'cache/', 0777, true ) === false ) {
-		?>
-		<li><strong>ERROR:</strong> folder <em>cache/</em> could not be created. Please check the permissions of the root folder and make sure we are allowed to write to it. we abort the setup here.</li>
-		<?php
-		exit;
-	} else {
-		?>
-		<li>folder <em>cache/</em> was created successfully</li>
-		<?php
-	}
-} else {
-	?><li>folder <em>cache/</em> already exists, we do not need to create it</li><?php
-}
-?>
-</ul>
+	</ul>
 
-
-<?php
-if( $testcontent ) {
-	?>
-	<h3>creating some test content:</h3>
+	<h3>checking <em>content/</em> folder:</h3>
 	<ul>
 	<?php
+}
+
+if( ! is_dir( $abspath.'content/') ) {
+
+	if( $output ) {
+		?>
+		<li>folder <em>content/</em> does not exist, trying to create it</li>
+		<?php
+	}
+
+	if( mkdir( $abspath.'content/', 0777, true ) === false ) {
+
+		if( $debug_output ) {
+			?>
+			<li><strong>ERROR:</strong> folder <em>content/</em> could not be created. Please check the permissions of the root folder and make sure we are allowed to write to it. we abort the setup here.</li>
+			<?php
+		}
+
+		exit;
+	} else {
+
+		if( $output ) {
+			?>
+			<li>folder <em>content/</em> was created successfully</li>
+			<?php
+		}
+
+	}
+} else {
+
+	if( $output ) {
+		?><li>folder <em>content/</em> already exists, we do not need to create it</li><?php
+	}
+
+}
+
+if( $output ) {
+	?>
+	</ul>
+
+
+	<h3>checking <em>cache/</em> folder:</h3>
+	<ul>
+	<?php
+}
+
+if( ! is_dir( $abspath.'cache/') ) {
+
+	if( $output ) {
+		?>
+		<li>folder <em>cache/</em> does not exist, trying to create it</li>
+		<?php
+	}
+
+	if( mkdir( $abspath.'cache/', 0777, true ) === false ) {
+
+		if( $debug_output ) {
+			?>
+			<li><strong>ERROR:</strong> folder <em>cache/</em> could not be created. Please check the permissions of the root folder and make sure we are allowed to write to it. we abort the setup here.</li>
+			<?php
+		}
+
+		exit;
+	} else {
+
+		if( $output ) {
+			?>
+			<li>folder <em>cache/</em> was created successfully</li>
+			<?php
+		}
+
+	}
+} else {
+
+	if( $output ) {
+		?><li>folder <em>cache/</em> already exists, we do not need to create it</li><?php
+	}
+
+}
+
+if( $output ) {
+	?>
+	</ul>
+	<?php
+}
+
+
+if( $testcontent ) {
+
+	if( $output ) {
+		?>
+		<h3>creating some test content:</h3>
+		<ul>
+		<?php
+	}
+
 	function setup_create_testcontent( $root, $file_structure ) {
 		if( $file_structure['type'] == 'file' ) {
 			$filename = $file_structure['name'];
 			$data = $file_structure['content'];
 			if( ! file_exists( $root.$filename ) ){
-				?>
-				<li>file <em><?= $root.$filename ?></em> does not exist, we need to create it</li>
-				<?php
-				if( file_put_contents( $root.$filename, $data ) === false ) {
+
+				if( $output ) {
 					?>
-					<li><strong>ERROR:</strong> could not create the file <em><?= $root.$filename ?></em>. we abort the setup here.</li>
+					<li>file <em><?= $root.$filename ?></em> does not exist, we need to create it</li>
 					<?php
+				}
+
+				if( file_put_contents( $root.$filename, $data ) === false ) {
+
+					if( $output ) {
+						?>
+						<li><strong>ERROR:</strong> could not create the file <em><?= $root.$filename ?></em>. we abort the setup here.</li>
+						<?php
+					}
+
 					exit;
 				}
 			} else {
-				?>
-				<li>file <em><?= $root.$filename ?></em> already exists, we do not need to create it</li>
-				<?php
+
+				if( $output ) {
+					?>
+					<li>file <em><?= $root.$filename ?></em> already exists, we do not need to create it</li>
+					<?php
+				}
+
 			}
 		} elseif( $file_structure['type'] == 'folder' ) {
 			$foldername = $file_structure['name'];
 			if( ! is_dir( $root.$foldername) ) {
-				?>
-				<li>folder <em><?= $root.$foldername ?></em> does not exist, we need to create it</li>
-				<?php
-				if( mkdir( $root.$foldername, 0777, true ) === false ) {
+
+				if( $output ) {
 					?>
-					<li><strong>ERROR:</strong> could not create the folder <em><?= $root.$foldername ?></em>. we abort the setup here.</li>
+					<li>folder <em><?= $root.$foldername ?></em> does not exist, we need to create it</li>
 					<?php
+				}
+
+				if( mkdir( $root.$foldername, 0777, true ) === false ) {
+
+					if( $debug_output ) {
+						?>
+						<li><strong>ERROR:</strong> could not create the folder <em><?= $root.$foldername ?></em>. we abort the setup here.</li>
+						<?php
+					}
+
 					exit;
 				}
 			} else {
-				?>
-				<li>folder <em><?= $root.$foldername ?></em> already exist, we do not need to create it</li>
-				<?php
+
+				if( $output ) {
+					?>
+					<li>folder <em><?= $root.$foldername ?></em> already exist, we do not need to create it</li>
+					<?php
+				}
+
 			}
 		}
 
@@ -280,40 +393,66 @@ if( $testcontent ) {
 		),
 	);
 	setup_create_testcontent( $abspath, $file_structure );
-	?>
-	<li>test folder structure created successfully</li>
-	</ul>
-	<?php
+
+	if( $output ) {
+		?>
+		<li>test folder structure created successfully</li>
+		</ul>
+		<?php
+	}
+
 }
 
-?>
 
-<h3>creating the <em>config.php</em> file:</h3>
-<ul>
-	<?php
+if( $output ) {
+	?>
+	<h3>creating the <em>config.php</em> file:</h3>
+	<ul>
+		<?php
+	}
+
 	if( $config ) {
 		$content = "<?php\r\n\r\nreturn [\r\n	'site_title' => '".$site_title."',\r\n	'auth_mail' => '".$auth_mail."',\r\n	'debug' => true,\r\n	'author' => [\r\n		'p-name' => '".$author_name."',\r\n	],\r\n];\r\n"; // CLEANUP: remove the debug option, when the system is stable enough
 		if( file_put_contents( $abspath.'config.php', $content ) === false ) {
-			?>
-			<li><strong>ERROR:</strong> could not create the file <em>config.php</em>. make sure the folder is writeable. we abort the setup here.</li>
-			<?php
+
+			if( $debug_output ) {
+				?>
+				<li><strong>ERROR:</strong> could not create the file <em>config.php</em>. make sure the folder is writeable. we abort the setup here.</li>
+				<?php
+			}
+			exit;
+
 		} else {
-			?>
-			<li>file <em>config.php</em> created successfully</li>
-			<?php
+
+			if( $output ) {
+				?>
+				<li>file <em>config.php</em> created successfully</li>
+				<?php
+			}
+
 		}
 	} else {
-		?>
-		<li>file <em>config.php</em> exists; if you need to recreate it, delete it and rerun this setup.</li>
-		<?php
+
+		if( $output ) {
+			?>
+			<li>file <em>config.php</em> exists; if you need to recreate it, delete it and rerun this setup.</li>
+			<?php
+		}
+
 	}
+
+if( $output ) {
 	?>
-</ul>
+	</ul>
 
 
-<hr>
-<h3>Setup finished!</h3>
-<p>please <a href="<?= $baseurl ?>">reload this page</a>.</p>
-<hr>
-<?php
-exit;
+	<hr>
+	<h3>Setup finished!</h3>
+	<p>please <a href="<?= $baseurl ?>">reload this page</a>.</p>
+	<hr>
+	<?php
+	
+	exit;
+}
+
+header( 'location: '.$baseurl );
